178122899974c4eb852283bbcd87c51d23501121
AlexSamak <2009kazan@gmail.com>
2020-10-08 17:49:38 +0400

    new chips

--- blog/tests/factories/blogs.py
+++ blog/tests/factories/blogs.py
@@ -1,4 +1,4 @@
-from factory import django, Faker, lazy_attribute
+from factory import django, Faker, lazy_attribute, post_generation
 from blog.models import Blog
 
 Faker._DEFAULT_LOCALE = 'ru_RU'
@@ -18,3 +18,12 @@ class BlogFactory(django.DjangoModelFactory):
         return (Faker('pydecimal', left_digits=3, right_digits=2,
                       positive=True, min_value=100,
                       max_value=900).generate({}), 'RUB')
+
+    @post_generation
+    def def_test(self, create, extracted, key1, key2, **kwargs):
+        if not create:
+            return
+        print(extracted)
+        print(key1)
+        print(key2)
+        return 48+54
--- blog/tests/test_models/test_blogs.py
+++ blog/tests/test_models/test_blogs.py
@@ -1,4 +1,5 @@
 # from django.urls import reverse
+from factory.faker import faker
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -35,7 +36,7 @@ class BlogTest(APITestCase):
     def test_blog_factory_found(self):
         blog = BlogFactory()
         print(f'TEST=3:\nBlog name: {blog.name}\nBlog body:'
-              f' {blog.body}\nBlog slug: {blog.slug} ')
+              f' {blog.body}\nBlog slug: {blog.slug}\nBlog cost: {Blog.cost} ')
         data = {'slug': blog.slug}
         print(f'TEST=3: blog id={blog.id}')
         # url = reverse('blog-get', args=(blog.slug,))
@@ -51,16 +52,26 @@ class BlogTest(APITestCase):
         url = reverse('blog-list')
         print(f'TEST=4: {url}')
         for i in Blog.objects.all():
-            print(i.cost)
+            print(f'Name: {i.name} Cost: {i.cost}')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 10)
 
     def test_blog_Z(self):
-        blog = BlogFactory(name='Z')
+        blog = BlogFactory(name='Z',
+                           def_test='Check wonderful decorator!!!',
+                           def_test__key1=159753,
+                           def_test__key2='CHUDO!!!')
         # url = reverse('blog-get', args=(blog.slug,))
         url = reverse('blog-detail', kwargs={'pk': blog.id})
         print(f'TEST=5: {url}')
         response = self.client.get(url, data={'slug': blog.slug})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json().get('name'), "Z")
+
+    def test_one(self):
+        prop = "username"
+        fake = faker.Faker()
+        print(fake.profile(fields=[prop])[prop])
+
+
